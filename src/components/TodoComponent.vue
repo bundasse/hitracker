141a471bdecd2ed1a9c043b885721d796ec93e80
bundasse <ymtotolove@gmail.com>
2025-09-28 19:09:48 +0900

    009 add버튼 동작 추가

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -1,9 +1,17 @@
 <script setup>
 import { ref } from 'vue';
 const editFlg = ref(false);
+const inputValue = ref('')
+const addListCount = ref(0)
 
 function setEditFlg(value) {
     editFlg.value = value
+    if(!value){
+        addListCount.value = 0
+    }
+}
+function setAddList() {
+    addListCount.value ++;
 }
 </script>
 <template>
@@ -12,7 +20,7 @@ function setEditFlg(value) {
         <div class="d-flex buttons">
             <button v-if="!editFlg" @click="setEditFlg(true)"><i class='bx bx-edit'></i></button>
               <template v-else>
-                  <button @click="setEditFlg(false)"><i class='bx bx-plus'></i></button>
+                  <button @click="setAddList()"><i class='bx bx-plus'></i></button>
                   <button @click="setEditFlg(false)"><i class='bx bx-save'></i></button>
                   <button @click="setEditFlg(false)"><i class='bx bx-x'></i></button>
               </template>
@@ -21,10 +29,16 @@ function setEditFlg(value) {
     <div>
         <ul class="todoList">
             <li class="d-flex">
-                <input type="checkbox" name="" id="" class="listCheckBox">
+                <input type="checkbox" class="listCheckBox">
                 <p class="listText">뭐시기저시기</p>
                 <button v-if="editFlg"><i class='bx bx-list-x'></i></button>
             </li>
+            <template v-for="num in addListCount" :key="num">
+                <li class="d-flex">
+                    <input type="checkbox" class="listCheckBox">
+                    <input type="text" ref="inputValue">
+                </li>
+            </template>
         </ul>
     </div>
 </template>
